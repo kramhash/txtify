79ec5e1a0edbf104109dd5c450337e0d148f2b14
kramhash <mark@katamari.co.jp>
2013-10-08 19:15:40 +0900

    change regular expression.

--- index.js
+++ index.js
@@ -17,13 +17,16 @@ function txtfiy(file) {
   }
 
   function format(text) {
+
     var matches = text.match(/<\!\-\- txtify \-\->\s*([\s\S]+)[\s\n]*<\!\-\- endtxtify \-\->/im);
     if (matches) {
       text = matches[1];
     }
     text = text.trim();
     text = text.replace(/\"/g, '\u005C\u0022');
-    text = '"' + text + '"'
+    text = text.replace(/^(.*)/gm, '"$1');
+    text = text.replace(/(.+)$/gm, '$1" +');
+    text = text.replace(/\+$/, '');
 
     return "module.exports=" + text + ";\n";
   }
